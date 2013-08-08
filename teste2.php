f131967f57589eebb36256e237c4f7229065b1db
Liciane Andreatta <licianecristina@gmail.com>
2013-08-07 23:30:27 -0300

    Classe

--- teste2.php
+++ teste2.php
@@ -1,2 +1,17 @@
 <?php
-	phpinfo();
+class MainAction extends Action {
+
+	var $strIdBody = "bHome";
+	var $titlePage = "Ebanx - ";
+	
+	function getResults() {
+		return array(
+			"redir-boleto-pay" => new RedirectResult("http://www.ebanx.com/")
+			//"redir-boleto-pay" => new RedirectResult("http://localhost/ebanx/web/ebmng/")
+		);
+	}
+
+	function execute() {
+		return "redir-boleto-pay";
+	}
+}
